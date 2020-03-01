83a6890aa77f10baec36d1e75618dbf0d78c14f0
unknown <tayssire@hotmail.com>
2020-03-01 13:00:39 +0100

    cors + route to get ingredients

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
+require("./config/mongo");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -14,6 +16,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// this rule allows the client app to exchange via http via the server (AJAX ... Axios)
+const corsOptions = {
+  origin: [process.env.CLIENT_URL],
+  /* credentials : Configures the Access-Control-Allow-Credentials CORS header. Set to true to pass the header, otherwise it is omitted  https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Credentials */
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+// cors middle on
+app.use(cors(corsOptions));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require("express");
 var router = express.Router();
+const IngredientModel = require("../models/Ingredient");
+const UserModel = require("../models/User");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.send({ title: "Express" });
+});
+
+/* GET ingredients list   */
+router.get("/ingredients", function(req, res) {
+  IngredientModel.find()
+    .then(ingredients => {
+      res.status(200).json(ingredients);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+/* GET favorite recipes */
+router.get("/favorites/:userId", (req, res) => {
+  UserModel.findById(req.params.userId)
+    .populate("favorites")
+    .then(recipes => {
+      console.log(recipes);
+      res.status(200).json(recipes);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
