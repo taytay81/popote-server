afeb013a62a9dcee7540d427d3c6071cd80a6c3c
andrewnunezbibby <andrew.bibby3@gmail.com>
2020-03-05 20:44:15 +0100

    changed the data type for the rating attribute of the review schema

--- models/Review.js
+++ models/Review.js
@@ -1,26 +1,25 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
-
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
-    body: {
-        type: String,
-        required: true
-    },
-    recipeId: { 
-        type: String,
-        required: true
-    },
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    rating: {
-        type: Schema.Types.Decimal128
-    },
-    ratingCount: {type: Number}
-})
+  body: {
+    type: String,
+    required: true
+  },
+  recipeId: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  rating: {
+    type: Number
+  },
+  ratingCount: { type: Number }
+});
 
-const Review = mongoose.model("Review", reviewSchema)
-module.exports = Review
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;
